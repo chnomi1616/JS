0f011b40e3afb96e791a62fc88234015834121cb
Muhammad Noman <m.nomi1616@gmail.com>
2025-04-16 19:01:42 +0500

    Updated datatype.js

--- datatype.js
+++ datatype.js
@@ -1,10 +1,8 @@
 let name = "noman";
 let age = 10;
 
-// A simple program to print a message
 console.log(`Hello, my name is ${name} and I am ${age} years old.`);
 
-// Check if the person is a minor or an adult
 if (age < 18) {
     console.log(`${name} is a minor.`);
 } else {
