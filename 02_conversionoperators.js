b822016954e0a34ac86701b1d99113cabaa28a31
Muhammad Noman <m.nomi1616@gmail.com>
2025-04-24 15:23:58 +0500

    conversion is clear

---
+++ 02_conversionoperators.js
@@ -0,0 +1,21 @@
+// 🧠 parseInt:
+
+// Bas start ka number dekhta hai
+
+// Baad mein agar kuch letters hain to ignore karta hai
+
+// Decimal ho to sirf integer part leta hai
+let str = "123";
+let num = parseInt(str); // 123
+console.log(num);        // Output: 123
+
+
+// 🧠 toString():
+
+// Kisi bhi value ko text mein convert karta hai
+
+// Mostly tab use hota hai jab number ko display karwana ho
+let nums = 456;
+let str1 = num.toString(); // "456"
+console.log(str);         // Output: "456"
+console.log(typeof str);  // Output: "string"
